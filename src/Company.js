eb88100eb730cb4ec1d90b762e43fa0deaca5b93
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-05-09 11:42:37 +0900

    change: classProps の各オブジェクトに label プロパティを追加。

--- src/Company.js
+++ src/Company.js
@@ -5,14 +5,34 @@ export default class Company extends FireModel {
   static useAutonumber = false;
   static logicalDelete = false;
   static classProps = {
-    name: { type: String, default: "", required: true },
-    nameKana: { type: String, default: "", required: true },
-    zipcode: { type: String, default: "", required: false },
-    prefecture: { type: String, default: "", required: false },
-    city: { type: String, default: "", required: false },
-    address: { type: String, default: "", required: false },
-    building: { type: String, default: "", required: false },
-    tel: { type: String, default: "", required: false },
-    fax: { type: String, default: "", required: false },
+    name: { type: String, label: "会社名", default: "", required: true },
+    nameKana: {
+      type: String,
+      label: "会社名（カナ)",
+      default: "",
+      required: true,
+    },
+    zipcode: { type: String, label: "郵便番号", default: "", required: false },
+    prefecture: {
+      type: String,
+      label: "都道府県",
+      default: "",
+      required: false,
+    },
+    city: { type: String, label: "市区町村", default: "", required: false },
+    address: {
+      type: String,
+      label: "町域名・番地",
+      default: "",
+      required: false,
+    },
+    building: {
+      type: String,
+      label: "建物名・階数",
+      default: "",
+      required: false,
+    },
+    tel: { type: String, label: "電話番号", default: "", required: false },
+    fax: { type: String, label: "FAX番号", default: "", required: false },
   };
 }
