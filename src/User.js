1b151e26fbb318ef7c88c8faa0de7d45c1c1372c
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-05-21 10:26:54 +0900

    update: classProps を更新

--- src/User.js
+++ src/User.js
@@ -3,28 +3,35 @@ import FireModel from "air-firebase-v2";
 export default class User extends FireModel {
   static collectionPath = "Users";
   static classProps = {
-    /** document-id */
-    docId: { type: String, default: false, required: false },
-
     /** Authentication's uid */
-    uid: { type: String, default: "", required: false },
+    uid: { type: String, default: "", label: "UID", required: false },
 
     /** email */
-    email: { type: String, default: "", required: true },
+    email: { type: String, default: "", label: "email", required: true },
 
     /** display name */
-    displayName: { type: String, default: "", required: false },
+    displayName: {
+      type: String,
+      default: "",
+      label: "表示名",
+      required: false,
+    },
 
     /**
      * employee-id
      * - Not required, as there may be users that do not fall under the employee category.
      * - 従業員には該当しないユーザーが存在する可能性もあるため、必須にはしない。
      */
-    employeeId: { type: String, default: "", required: false },
+    employeeId: {
+      type: String,
+      default: "",
+      label: "従業員ID",
+      required: false,
+    },
 
     /**
      * アプリの仕様権限
      */
-    roles: { type: Array, default: () => [], required: false },
+    roles: { type: Array, default: () => [], label: "権限", required: false },
   };
 }
