de38feef12c2a016d7975223545ba74fb8762731
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-05-29 11:16:15 +0900

    commit

--- src/Employee.js
+++ src/Employee.js
@@ -10,6 +10,9 @@ export default class Employee extends FireModel {
     code: defField("code", { label: "従業員コード" }),
     lastName: defField("lastName", { required: true }),
     firstName: defField("firstName", { required: true }),
+    lastNameKana: defField("lastNameKana", { required: true }),
+    firstNameKana: defField("firstNameKana", { required: true }),
+    displayName: defField("displayName", { required: true }),
     isForeigner: defField("isForeigner"),
     foreignName: defField("foreignName"),
     nationality: defField("nationality"),
@@ -19,5 +22,45 @@ export default class Employee extends FireModel {
     Object.defineProperties(this, {
       fullName: fullName(),
     });
+  }
+
+  /**
+   * 外国籍の場合の必須フィールド（外国人名、国籍）を検証します。
+   * エラーがある場合は例外をスローします。
+   */
+  _validateForeignerRequiredFields() {
+    if (this.isForeigner) {
+      if (!this.foreignName) {
+        throw new Error(
+          "[Employee.js] foreignName is required when isForeigner is true."
+        );
+      }
+      if (!this.nationality) {
+        throw new Error(
+          "[Employee.js] nationality is required when isForeigner is true."
+        );
+      }
+    }
+  }
+
+  /**
+   * 新しい従業員ドキュメントが作成される前に実行されるフック。
+   * - 親クラスの `beforeCreate` を呼び出します。
+   * - 従業員が外国人の場合、外国人名と国籍が未入力であればエラーをスローします。
+   */
+  async beforeCreate() {
+    // 親クラスの beforeCreate フックを実行
+    await super.beforeCreate();
+    this._validateForeignerRequiredFields();
+  }
+
+  /**
+   * 従業員ドキュメントが更新される前に実行されるフック。
+   * - 親クラスの `beforeUpdate` を呼び出します。
+   * - 従業員が外国人の場合、外国人名と国籍が未入力であればエラーをスローします。
+   */
+  async beforeUpdate() {
+    await super.beforeUpdate();
+    this._validateForeignerRequiredFields();
   }
 }
--- src/parts/definitions/name.js
+++ src/parts/definitions/name.js
@@ -10,6 +10,18 @@ export const name = {
   },
 };
 
+export const displayName = {
+  type: String,
+  default: "",
+  label: "表示名",
+  length: 6,
+  required: undefined,
+  component: {
+    name: "air-text-field",
+    attrs: {},
+  },
+};
+
 export const foreignName = {
   type: String,
   default: "",
@@ -48,6 +60,20 @@ export const lastName = {
   },
 };
 
+export const lastNameKana = {
+  type: String,
+  default: "",
+  label: "セイ",
+  length: 40,
+  required: undefined,
+  component: {
+    name: "air-text-field",
+    attrs: {
+      inputType: "katakana",
+    },
+  },
+};
+
 export const firstName = {
   type: String,
   default: "",
@@ -57,6 +83,20 @@ export const firstName = {
   component: {
     name: "air-text-field",
     attrs: {},
+  },
+};
+
+export const firstNameKana = {
+  type: String,
+  default: "",
+  label: "メイ",
+  length: 40,
+  required: undefined,
+  component: {
+    name: "air-text-field",
+    attrs: {
+      inputType: "katakana",
+    },
   },
 };
 
