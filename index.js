f54f4177d7cd7098a7163b08d44976e627114e96
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-06-20 11:45:56 +0900

    add: OperationResult クラスを仮実装

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 export { default as Company } from "./src/Company.js";
 export { default as Customer, CustomerMinimal } from "./src/Customer.js";
 export { default as Employee } from "./src/Employee.js";
+export { default as OperationResult } from "./src/OperationResult.js";
 export { default as Site } from "./src/Site.js";
 export { default as SiteOperationSchedule } from "./src/SiteOperationSchedule.js";
 export { default as User } from "./src/User.js";
---
+++ src/OperationResult.js
@@ -0,0 +1,38 @@
+import FireModel from "air-firebase-v2";
+import { defField } from "./parts/fieldDefinitions.js";
+import Site from "./Site.js";
+
+export default class OperationResult extends FireModel {
+  static collectionPath = "OperationResults";
+  static useAutonumber = false;
+  static logicalDelete = false;
+  static classProps = {
+    siteId: defField("oneLine", {
+      label: "現場",
+      required: true,
+      component: {
+        name: "air-autocomplete-api",
+        attrs: {
+          api: () => {
+            return async (search) =>
+              await new Site().fetchDocs({ constraints: search });
+          },
+          itemValue: "docId",
+          itemTitle: "name",
+          noFilter: true,
+        },
+      },
+    }),
+    date: defField("date", { label: "日付", required: true }),
+    /**
+     * workers
+     * { isEmployee, employeeId | outsourcerId, startAt, endAt, isQualificated, isOjt }
+     */
+    workers: {
+      type: Array,
+      default: () => [],
+      label: "稼働明細",
+      required: false,
+    },
+  };
+}
