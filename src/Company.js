bdd3d9047f094da1fe7b687d1bbefeef20be8997
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-07-31 09:26:10 +0900

    add: SiteOrder management methods to Company class

--- src/Company.js
+++ src/Company.js
@@ -2,6 +2,7 @@ import FireModel from "air-firebase-v2";
 import { defField } from "./parts/fieldDefinitions.js";
 import { defAccessor } from "./parts/accessorDefinitions.js";
 import Agreement from "./Agreement.js";
+import SiteOrder from "./SiteOrder.js";
 
 export default class Company extends FireModel {
   static className = "会社";
@@ -27,6 +28,14 @@ export default class Company extends FireModel {
       label: "既定の取極め",
       customClass: Agreement,
     }),
+    /**
+     * Field to manage the display order of site-shift type pairs for placement management.
+     * Format: { siteId, shiftType }
+     */
+    siteOrder: defField("array", {
+      customClass: SiteOrder,
+      hidden: true,
+    }),
   };
 
   afterInitialize() {
@@ -34,5 +43,53 @@ export default class Company extends FireModel {
       fullAddress: defAccessor("fullAddress"),
       prefecture: defAccessor("prefecture"),
     });
+  }
+
+  /**
+   * Adds a new SiteOrder to the siteOrder array.
+   * @param {string} siteId - The ID of the site.
+   * @param {string} shiftType - The shift type to associate with the site.
+   * @param {number} [index=-1] - The position to insert the new SiteOrder. Defaults to the end.
+   */
+  addSiteOrder(siteId, shiftType, index = -1) {
+    const newOrder = new SiteOrder({ siteId, shiftType });
+    if (this.siteOrder.some((order) => order.key === newOrder.key)) {
+      throw new Error(`SiteOrder with key ${newOrder.key} already exists.`);
+    }
+    if (index === -1) {
+      this.siteOrder.push(newOrder);
+    } else {
+      this.siteOrder.splice(index, 0, newOrder);
+    }
+  }
+
+  /**
+   * Changes the order of a SiteOrder in the siteOrder array.
+   * @param {number} oldIndex - The current index of the SiteOrder.
+   * @param {number} newIndex - The new index to move the SiteOrder to.
+   */
+  changeSiteOrder(oldIndex, newIndex) {
+    if (
+      oldIndex < 0 ||
+      oldIndex >= this.siteOrder.length ||
+      newIndex < 0 ||
+      newIndex >= this.siteOrder.length
+    ) {
+      throw new Error("Invalid index for site order change.");
+    }
+    const [movedOrder] = this.siteOrder.splice(oldIndex, 1);
+    this.siteOrder.splice(newIndex, 0, movedOrder);
+  }
+
+  /**
+   * Removes a SiteOrder from the siteOrder array.
+   * @param {string} key - The key of the SiteOrder to remove.
+   */
+  removeSiteOrder(key) {
+    const index = this.siteOrder.findIndex((order) => order.key === key);
+    if (index === -1) {
+      throw new Error(`SiteOrder with key ${key} does not exist.`);
+    }
+    this.siteOrder.splice(index, 1);
   }
 }
