02a46e61ce51ea1ba7303377d12af531ce2f55e0
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-08-18 18:53:33 +0900

    commit

--- src/constants/shift-type.js
+++ src/constants/shift-type.js
@@ -4,12 +4,17 @@ export const SHIFT_TYPE_DAY = "DAY";
 export const SHIFT_TYPE_NIGHT = "NIGHT";
 
 export const SHIFT_TYPE = Object.freeze({
-  DAY: "日勤",
-  NIGHT: "夜勤",
+  DAY: { title: "日勤", color: "deep-orange" },
+  NIGHT: { title: "夜勤", color: "indigo" },
 });
 
 export const SHIFT_TYPE_ARRAY = Object.entries(SHIFT_TYPE).map(
   ([key, value]) => {
-    return { value: key, title: value };
+    return { value: key, title: value.title, color: value.color };
   }
 );
+
+export const SHIFT_TYPE_VALIDATOR = (value) => {
+  const arr = Object.keys(SHIFT_TYPE);
+  return arr.includes(value);
+};
