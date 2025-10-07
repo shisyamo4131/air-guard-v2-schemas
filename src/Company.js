37177279bb29dc2e6e4c4dd7f12b44aa752f79a2
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-10-07 16:57:12 +0900

    change:
    - addSiteOrder, changeSiteOrder, removeSiteOrder メソッドを廃止。
    - 代わりに siteOrder.add, siteOrder.change, siteOrder.remove メソッドを実装。

--- src/Company.js
+++ src/Company.js
@@ -50,75 +50,94 @@ export default class Company extends FireModel {
       fullAddress: defAccessor("fullAddress"),
       prefecture: defAccessor("prefecture"),
     });
+
+    /*************************************************************************
+     * CUSTOM METHODS FOR siteOrder ARRAY
+     * Note: These methods modify the siteOrder array directly.
+     * The Company instance itself is not updated by these methods.
+     * Use the provided public methods to interact with siteOrder.
+     *************************************************************************/
+    Object.defineProperties(this.siteOrder, {
+      /**
+       * Inserts a new SiteOrder into the `siteOrder` array.
+       * @param {Object} params - The parameters for the SiteOrder.
+       * @param {string} params.siteId - The ID of the site.
+       * @param {string} params.shiftType - The shift type associated with the site.
+       * @param {number} [index=-1] - The position to insert the new SiteOrder. Defaults to the end.
+       * @returns {SiteOrder} The newly created SiteOrder instance.
+       * @throws {Error} If a SiteOrder with the same key already exists.
+       */
+      add: {
+        value: function ({ siteId, shiftType }, index = -1) {
+          const newOrder = new SiteOrder({ siteId, shiftType });
+          if (this.some((order) => order.key === newOrder.key)) {
+            throw new Error(
+              `SiteOrder with key ${newOrder.key} already exists.`
+            );
+          }
+          index === -1 ? this.push(newOrder) : this.splice(index, 0, newOrder);
+          return newOrder;
+        },
+        writable: false,
+        enumerable: false,
+      },
+      /**
+       * Changes the order of a SiteOrder in the siteOrder array.
+       * Note: Company instance does not be updated by this method.
+       * @param {number} oldIndex - The current index of the SiteOrder.
+       * @param {number} newIndex - The new index to move the SiteOrder to.
+       * @returns {void}
+       * @throws {Error} If oldIndex or newIndex are out of bounds.
+       */
+      change: {
+        value: function (oldIndex, newIndex) {
+          const length = this.length;
+          if (oldIndex < 0 || oldIndex >= length) {
+            throw new Error("Invalid oldIndex for site order change.");
+          }
+          if (newIndex < 0 || newIndex >= length) {
+            throw new Error("Invalid newIndex for site order change.");
+          }
+          const [movedOrder] = this.splice(oldIndex, 1);
+          this.splice(newIndex, 0, movedOrder);
+        },
+        writable: false,
+        enumerable: false,
+      },
+      /**
+       * Removes a SiteOrder from the siteOrder array.
+       * Note: Company instance does not be updated by this method.
+       * @param {string|Object} arg - The key or {siteId, shiftType} of the SiteOrder to remove.
+       * @returns {void}
+       * @throws {Error} If the SiteOrder does not exist or if the argument is invalid.
+       */
+      remove: {
+        value: function (arg) {
+          let key = "";
+          if (typeof arg === "string") {
+            key = arg;
+          } else if (typeof arg === "object" && arg.siteId && arg.shiftType) {
+            key = `${arg.siteId}-${arg.shiftType}`;
+          } else {
+            throw new Error(
+              "Invalid argument for remove. Must be a string key or an object with siteId and shiftType."
+            );
+          }
+
+          const index = this.findIndex((order) => order.key === key);
+          if (index === -1) {
+            throw new Error(`SiteOrder with key ${key} does not exist.`);
+          }
+
+          this.splice(index, 1);
+        },
+        writable: false,
+        enumerable: false,
+      },
+    });
   }
 
   /***************************************************************************
    * PUBLIC METHODS
    ***************************************************************************/
-  /**
-   * Inserts a new SiteOrder into the `siteOrder` array.
-   * Note: Company instance does not be updated by this method.
-   * @param {Object} params - The parameters for the SiteOrder.
-   * @param {string} params.siteId - The ID of the site.
-   * @param {string} params.shiftType - The shift type associated with the site.
-   * @param {number} [index=-1] - The position to insert the new SiteOrder. Defaults to the end.
-   */
-  addSiteOrder({ siteId, shiftType }, index = -1) {
-    const newOrder = new SiteOrder({ siteId, shiftType });
-    if (this.siteOrder.some((order) => order.key === newOrder.key)) {
-      throw new Error(`SiteOrder with key ${newOrder.key} already exists.`);
-    }
-    index === -1
-      ? this.siteOrder.push(newOrder)
-      : this.siteOrder.splice(index, 0, newOrder);
-  }
-
-  /**
-   * Changes the order of a SiteOrder in the siteOrder array.
-   * Note: Company instance does not be updated by this method.
-   * @param {number} oldIndex - The current index of the SiteOrder.
-   * @param {number} newIndex - The new index to move the SiteOrder to.
-   */
-  changeSiteOrder(oldIndex, newIndex) {
-    const length = this.siteOrder.length;
-    if (oldIndex < 0 || oldIndex >= length) {
-      throw new Error("Invalid oldIndex for site order change.");
-    }
-    if (newIndex < 0 || newIndex >= length) {
-      throw new Error("Invalid newIndex for site order change.");
-    }
-    const [movedOrder] = this.siteOrder.splice(oldIndex, 1);
-    this.siteOrder.splice(newIndex, 0, movedOrder);
-  }
-
-  /**
-   * Removes a SiteOrder from the siteOrder array.
-   * Note: Company instance does not be updated by this method.
-   * @param {string|Object} arg - The key or {siteId, shiftType} of the SiteOrder to remove.
-   */
-  removeSiteOrder(arg) {
-    // Determine the key based on the argument type.
-    // If it's a string, use it directly. If it's an object, construct the key.
-    // Throw an error if the argument is invalid.
-    let key = "";
-    if (typeof arg === "string") {
-      key = arg;
-    } else if (typeof arg === "object" && arg.siteId && arg.shiftType) {
-      key = `${arg.siteId}-${arg.shiftType}`;
-    } else {
-      throw new Error(
-        "Invalid argument for removeSiteOrder. Must be a string key or an object with siteId and shiftType."
-      );
-    }
-
-    // Find the index of the SiteOrder with the specified key.
-    // Throw an error if it does not exist.
-    const index = this.siteOrder.findIndex((order) => order.key === key);
-    if (index === -1) {
-      throw new Error(`SiteOrder with key ${key} does not exist.`);
-    }
-
-    // Remove the SiteOrder from the array.
-    this.siteOrder.splice(index, 1);
-  }
 }
