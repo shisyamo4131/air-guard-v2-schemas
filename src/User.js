8424091197b802fa323051282aac025da0c88a07
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-10-09 17:14:33 +0900

    commit

--- src/User.js
+++ src/User.js
@@ -1,31 +1,37 @@
+/*****************************************************************************
+ * User Model ver 1.0.0
+ * @author shisyamo4131
+ *****************************************************************************/
 import FireModel from "air-firebase-v2";
 import { defField } from "./parts/fieldDefinitions.js";
+
+const classProps = {
+  email: defField("email", { required: true }),
+  displayName: defField("displayName", { required: true }),
+  /**
+   * employee-id
+   * - 従業員には該当しないユーザーが存在する可能性もあるため、必須にはしない。
+   */
+  employeeId: defField("oneLine", { label: "従業員ID", hidden: true }),
+  roles: {
+    type: Array,
+    default: () => [],
+    label: "権限",
+    required: false,
+    hidden: true,
+  },
+  disabled: defField("check", {
+    label: "使用不可",
+    default: false,
+    required: false,
+    hidden: true,
+  }),
+};
 
 export default class User extends FireModel {
   static className = "ユーザー";
   static collectionPath = "Users";
-  static classProps = {
-    email: defField("email", { required: true }),
-    displayName: defField("displayName", { required: true }),
-    /**
-     * employee-id
-     * - 従業員には該当しないユーザーが存在する可能性もあるため、必須にはしない。
-     */
-    employeeId: defField("oneLine", { label: "従業員ID", hidden: true }),
-    roles: {
-      type: Array,
-      default: () => [],
-      label: "権限",
-      required: false,
-      hidden: true,
-    },
-    disabled: defField("check", {
-      label: "使用不可",
-      default: false,
-      required: false,
-      hidden: true,
-    }),
-  };
+  static classProps = classProps;
 
   static headers = [
     { title: "email", key: "email" },
