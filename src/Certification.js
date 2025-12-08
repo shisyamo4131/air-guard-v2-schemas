5a079274a07f827fa0da84f2cf794f0389d3023c
shisyamo4131 <sevenstar_1226@hotmail.com>
2025-12-08 17:38:17 +0900

    資格モデルのプロパティに必須フィールドを追加し、keyプロパティを定義

--- src/Certification.js
+++ src/Certification.js
@@ -3,6 +3,9 @@
  * @version 1.0.0
  * @author shisyamo4131
  * @description 警備業務資格モデル
+ *
+ * NOTE: 配列で管理されるので key が必要。2025-12-08 現在、資格名が重複することはない想定。
+ * 但し、2バイト文字が key として適切かは要検討。
  */
 import { BaseClass } from "@shisyamo4131/air-firebase-v2";
 import { defField } from "./parts/fieldDefinitions.js";
@@ -11,7 +14,7 @@ const classProps = {
   name: defField("name", { label: "資格名", required: true }),
   type: defField("certificationType", { required: true }),
   issuedBy: defField("name", { label: "発行元" }),
-  issueDateAt: defField("dateAt", { label: "取得日" }),
+  issueDateAt: defField("dateAt", { label: "取得日", required: true }),
   expirationDateAt: defField("dateAt", { label: "有効期限" }),
   serialNumber: defField("oneLine", { label: "証明書番号" }),
 };
@@ -23,8 +26,24 @@ const classProps = {
  * @prop {Date} issueDateAt - 取得日
  * @prop {Date} expirationDateAt - 有効期限
  * @prop {string} serialNumber - 証明書番号
+ *
+ * @prop {string} key - 資格名 (nameと同じ) (読み取り専用)
  */
 export default class Certification extends BaseClass {
   static className = "資格";
   static classProps = classProps;
+
+  afterInitialize(item = {}) {
+    super.afterInitialize(item);
+    Object.defineProperties(this, {
+      key: {
+        configurable: true,
+        enumerable: true,
+        get() {
+          return this.name;
+        },
+        set() {},
+      },
+    });
+  }
 }
