515c581d20000dcfee9d25fe7058359f5a19011c
shisyamo4131 <sevenstar_1226@hotmail.com>
2026-01-06 14:55:33 +0900

    commit

--- src/constants/day-of-week.js
+++ src/constants/day-of-week.js
@@ -1,20 +1,20 @@
 // prettier-ignore
 export const VALUES = Object.freeze({
-  SUNDAY: { value: 0, title: "日曜日" },
-  MONDAY: { value: 1, title: "月曜日" },
-  TUESDAY: { value: 2, title: "火曜日" },
-  WEDNESDAY: { value: 3, title: "水曜日" },
-  THURSDAY: { value: 4, title: "木曜日" },
-  FRIDAY: { value: 5, title: "金曜日" },
-  SATURDAY: { value: 6, title: "土曜日" },
+  0: { value: 0, title: "日曜日" },
+  1: { value: 1, title: "月曜日" },
+  2: { value: 2, title: "火曜日" },
+  3: { value: 3, title: "水曜日" },
+  4: { value: 4, title: "木曜日" },
+  5: { value: 5, title: "金曜日" },
+  6: { value: 6, title: "土曜日" },
 });
 
 export const OPTIONS = [
-  { title: VALUES.SUNDAY.title, value: VALUES.SUNDAY.value },
-  { title: VALUES.MONDAY.title, value: VALUES.MONDAY.value },
-  { title: VALUES.TUESDAY.title, value: VALUES.TUESDAY.value },
-  { title: VALUES.WEDNESDAY.title, value: VALUES.WEDNESDAY.value },
-  { title: VALUES.THURSDAY.title, value: VALUES.THURSDAY.value },
-  { title: VALUES.FRIDAY.title, value: VALUES.FRIDAY.value },
-  { title: VALUES.SATURDAY.title, value: VALUES.SATURDAY.value },
+  { title: VALUES[0].title, value: VALUES[0].value },
+  { title: VALUES[1].title, value: VALUES[1].value },
+  { title: VALUES[2].title, value: VALUES[2].value },
+  { title: VALUES[3].title, value: VALUES[3].value },
+  { title: VALUES[4].title, value: VALUES[4].value },
+  { title: VALUES[5].title, value: VALUES[5].value },
+  { title: VALUES[6].title, value: VALUES[6].value },
 ];
