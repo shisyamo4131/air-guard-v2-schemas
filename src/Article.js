7b97d930cb088fa99a1e409efc9dff197458e940
shisyamo4131 <sevenstar_1226@hotmail.com>
2026-05-29 14:12:34 +0900

    Update Article class: rename labels to 商品, allow negative price

--- src/Article.js
+++ src/Article.js
@@ -10,30 +10,30 @@ import { VALIDATION_ERRORS } from "./errorDefinitions.js";
  * @extends FireModel
  *
  * @description
- * 稼働外で計上すべき売上（請求）の品目マスタクラスです。
+ * 稼働外で計上すべき売上（請求）の商品マスタクラスです。
  * 無線機利用料、指名料など、作業員の稼働実績とは無関係に発生する請求項目を管理します。
  *
- * @property {string} code - 品目コード
- * @property {string} name - 品目名
- * @property {string} description - 品目説明
+ * @property {string} code - 商品コード
+ * @property {string} name - 商品名
+ * @property {string} description - 商品説明
  * @property {number} price - 単価
  * @property {string} remarks - 備考
  *****************************************************************************/
 export default class Article extends FireModel {
-  static className = "品目";
+  static className = "商品";
   static collectionPath = "Articles";
   static useAutonumber = false;
   static logicalDelete = true;
 
   static classProps = {
-    code: defField("code", { label: "品目コード" }),
+    code: defField("code", { label: "商品コード" }),
     name: defField("name", {
-      label: "品目名",
+      label: "商品名",
       required: true,
       length: 50,
     }),
     description: defField("description", {
-      label: "品目説明",
+      label: "商品説明",
     }),
     price: defField("price", {
       label: "単価",
@@ -50,15 +50,7 @@ export default class Article extends FireModel {
             { ja: "単価は有効な数値である必要があります。" },
           );
         }
-        if (value < 0) {
-          return VALIDATION_ERRORS.MIN_VALUE_ERROR(0);
-        }
         return true;
-      },
-      component: {
-        attrs: {
-          min: 0,
-        },
       },
     }),
     remarks: defField("remarks"),
@@ -68,7 +60,7 @@ export default class Article extends FireModel {
 
   static headers = [
     { title: "コード", key: "code" },
-    { title: "品目名", key: "name" },
+    { title: "商品名", key: "name" },
     { title: "単価", key: "price" },
   ];
 }
