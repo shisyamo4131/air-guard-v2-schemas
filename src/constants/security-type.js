7093c5fa54f16ea8d5045fede0a8bf0c87d694c8
shisyamo4131 <sevenstar_1226@hotmail.com>
2026-06-29 17:06:32 +0900

    commit

--- src/constants/security-type.js
+++ src/constants/security-type.js
@@ -1,60 +1,81 @@
 /*****************************************************************************
  * @file ./src/constants/security-type.js
  * @description 警備種別定数定義
- *
- * - title: 表示に使用される文字列
- * - value: 内部値
- * - aggregation: 集計対象かどうかのフラグ群
- *   - sales: 売上集計対象かどうかのフラグ
- *   - operationQuantity: 作業量集計対象かどうかのフラグ
- *
- * [更新履歴]
- * - 2026-06-26 `aggregation` を追加。
  *****************************************************************************/
+
 export const VALUES = Object.freeze({
   UNSET: {
     title: "未設定",
     value: "UNSET",
+    chart: {
+      backgroundColor: "rgba(158, 158, 158, 0.2)",
+      borderColor: "rgba(158, 158, 158, 1)",
+    },
     aggregation: {
       sales: true,
       operationQuantity: true,
     },
   },
+
   FACILITY: {
     title: "施設警備",
     value: "FACILITY",
+    chart: {
+      backgroundColor: "rgba(33, 150, 243, 0.2)",
+      borderColor: "rgba(33, 150, 243, 1)",
+    },
     aggregation: {
       sales: true,
       operationQuantity: true,
     },
   },
+
   CROWD: {
     title: "雑踏警備",
     value: "CROWD",
+    chart: {
+      backgroundColor: "rgba(255, 152, 0, 0.2)",
+      borderColor: "rgba(255, 152, 0, 1)",
+    },
     aggregation: {
       sales: true,
       operationQuantity: true,
     },
   },
+
   TRAFFIC: {
     title: "交通誘導",
     value: "TRAFFIC",
+    chart: {
+      backgroundColor: "rgba(76, 175, 80, 0.2)",
+      borderColor: "rgba(76, 175, 80, 1)",
+    },
     aggregation: {
       sales: true,
       operationQuantity: true,
     },
   },
+
   TRAINING: {
     title: "研修",
     value: "TRAINING",
+    chart: {
+      backgroundColor: "rgba(121, 85, 72, 0.2)",
+      borderColor: "rgba(121, 85, 72, 1)",
+    },
     aggregation: {
       sales: false,
       operationQuantity: false,
     },
   },
+
   OTHER: {
     title: "その他",
     value: "OTHER",
+    chart: {
+      backgroundColor: "rgba(156, 39, 176, 0.2)",
+      borderColor: "rgba(156, 39, 176, 1)",
+    },
     aggregation: {
       sales: true,
       operationQuantity: true,
